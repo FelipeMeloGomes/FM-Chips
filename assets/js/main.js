1c64e1e06aef6d6c52e188fa552983b0f3700be8
Felipe Melo <felipemelogomes36@gmail.com>
2024-04-16 18:47:38 -0300

    feat: finally front end

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-/*=============== SHOW MENU ===============*/
+/* SHOW MENU */
 const navMenu = document.getElementById("nav-menu"),
     navToggle = document.getElementById("nav-toggle"),
     navClose = document.getElementById("nav-close");
@@ -17,33 +17,80 @@ if (navClose) {
     });
 }
 
-/*=============== REMOVE MENU MOBILE ===============*/
+/* REMOVE MENU MOBILE */
 const navLink = document.querySelectorAll(".nav__link");
 
 const linkAction = () => {
     const navMenu = document.getElementById("nav-menu");
-    // When we click on each nav__link, we remove the show-menu class
     navMenu.classList.remove("show-menu");
 };
 navLink.forEach((n) => n.addEventListener("click", linkAction));
-/*=============== SHADOW HEADER ===============*/
+/* SHADOW HEADER */
 const shadowHeader = () => {
     const header = document.getElementById("header");
-    // Add a class if the bottom offset is greater than 50 of the viewport
     this.scrollY >= 50
         ? header.classList.add("shadow-header")
         : header.classList.remove("shadow-header");
 };
 window.addEventListener("scroll", shadowHeader);
-/*=============== SWIPER FAVORITES ===============*/
+/* SWIPER FAVORITES */
 const swiperFavorites = new Swiper(".favorites__swiper", {
     loop: "true",
     grabCursor: true,
     slidesPerView: "auto",
     centeredSlides: "auto",
 });
-/*=============== SHOW SCROLL UP ===============*/
+/* SHOW SCROLL UP */
+const scrollUp = () => {
+    const scrollUp = document.getElementById("scroll-up");
 
-/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+    this.scrollY >= 350
+        ? scrollUp.classList.add("show-scroll")
+        : scrollUp.classList.remove("show-scroll");
+};
+window.addEventListener("scroll", scrollUp);
+/* SCROLL SECTIONS ACTIVE LINK */
+const sections = document.querySelectorAll("section[id]");
 
-/*=============== SCROLL REVEAL ANIMATION ===============*/
+const scrollActive = () => {
+    const scrollDown = window.scrollY;
+
+    sections.forEach((current) => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute("id"),
+            sectionsClass = document.querySelector(
+                ".nav__menu a[href*=" + sectionId + "]"
+            );
+
+        if (
+            scrollDown > sectionTop &&
+            scrollDown <= sectionTop + sectionHeight
+        ) {
+            sectionsClass.classList.add("active-link");
+        } else {
+            sectionsClass.classList.remove("active-link");
+        }
+    });
+};
+window.addEventListener("scroll", scrollActive);
+/* SCROLL REVEAL ANIMATION */
+const sr = ScrollReveal({
+    origin: "top",
+    distance: "60px",
+    duration: 2500,
+    delay: 300,
+    reset: true,
+});
+sr.reveal(`.home__data, .favorites__container, .footer__container`);
+sr.reveal(`.home__data`);
+sr.reveal(`.home__circle, .home__img`, { delay: 600, scale: 0.5 });
+sr.reveal(`.home__chips-1, .home__chips-2, .home__chips-3`, {
+    delay: 1000,
+    interval: 1000,
+});
+sr.reveal(`.home__leaf`, { delay: 1200 });
+sr.reveal(`.home__tomato-1, .home__tomato-2`, { delay: 1400, interval: 100 });
+sr.reveal(`.care__img, .contact__img`, { origin: "left" });
+sr.reveal(`.care__list, .contact__data`, { origin: "right" });
+sr.reveal(`.banner__item, .products__card`, { interval: 100 });
